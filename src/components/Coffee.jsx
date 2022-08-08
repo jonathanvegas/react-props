0c578351e6084e43df47affe5acab6a2f32e3586
Jonathan Vegas <jonathanvegas@gmail.com>
2022-08-08 16:53:26 -0400

    fetch data from json online

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -2,23 +2,39 @@ import React, {useState} from "react";
 
 function Coffee(){
   const [coffeList, setCoffeeList] = useState();
-  const getCoffee = (temperature) => {
-    //fetch coffee api
-    fetch(`https://api.sampleapis.com/coffee/${temperature}`)
-      .then(results => results.json())
-      .then(data => setCoffeeList(data))
-      .catch(err => alert(err))
+  
+  // const getCoffee = (temperature) => {
+  //   //fetch coffee api
+  //   fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+  //     .then(results => results.json())
+  //     .then(data => setCoffeeList(data))
+  //     .catch(err => alert(err))
+  // }
+
+  const getCoffeeSync = async (temperature) => {
+    try{
+      const results = await fetch(`https://api.sampleapis.com/coffee/${temperature}`);
+      const data = await results.json();
+      setCoffeeList(data);
+    }
+    catch(err){
+      alert(err);
+    }
   }
+
   return(
     <>
-      <button onClick={() => getCoffee('hot')}>HOT</button>
-      <button onClick={() => getCoffee('iced')}>ICED</button> 
+      <button onClick={() => getCoffeeSync('hot')}>HOT</button>
+      <button onClick={() => getCoffeeSync('iced')}>ICED</button> 
       <section>
         <h2>Coffee List</h2>
         {!coffeList
           ? <p>Loading...</p>
-          : coffeList.map(coffee => (
-            <p key={coffee.id}>{coffee.title}: {coffee.description}</p>
+          : coffeList.map(coffee => ( 
+            <div>
+              <p fom key={coffee.id}>{coffee.title}</p>
+              {coffee.description}
+            </div>
           ))    
         }  
       </section>   
